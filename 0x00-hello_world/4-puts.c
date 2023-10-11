3f08b501f115d360a9e9ff076233a9d6f87a5ccb
1marvis <marmarvis6@gmail.com>
2023-10-11 15:49:30 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ * Description: this program prints output to the screen using the puts function
+ * Return: Always 0 (success)
+ */
+
+
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+
+}
